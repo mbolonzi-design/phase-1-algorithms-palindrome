908d913a1a6a521e3e0abb13ff44d1ff46454a8f
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-18 10:52:15 +0300

    commit tree 3edb34284de25eb4b2fe93e4b652fb9f5306d978

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindromeWord = word.split('').reverse().join('')
+  if(word === palindromeWord){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  The function takes one argument
+If the argument passed is a palindrome it returns a true value else return false
 */
+
 
 /*
   Add written explanation of your solution here
+  Add written explanation of your solution here
+  I declared a new variable palindromeWord 
+  it splits reverses and joins the word passed back to one word
+  after the split, reverse and join it is the compared to the word
+  If they match it returns true else returns false
 */
 
 // You can run `node index.js` to view these console logs
